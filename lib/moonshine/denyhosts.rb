1b308c4e0f604b8c4792f6361a993eef799d6835
Rails Machine Ops <ops@railsmachine.com>
2012-09-11 19:00:06 +0000

    Don't try to repopulate hosts.deny if you haven't explicitly denied something. This prevents clearing out things blocked by denyhosts.

--- lib/moonshine/denyhosts.rb
+++ lib/moonshine/denyhosts.rb
@@ -1,16 +1,16 @@
 module Moonshine
   module Denyhosts
     def denyhosts(options = {})
-      allow_list = options[:allow] || []
-      deny_list  = options[:deny] || []
-
       package 'denyhosts', :ensure => :installed
       service 'denyhosts', :ensure => :running, :require => package('denyhosts')
 
       %w(allow deny).each do |k|
-        file "/etc/hosts.#{k}",
-          :ensure  => :present,
-          :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', "hosts.#{k}.erb"), binding)
+        entries = options[k.to_sym] || []
+        if entries.any?
+          file "/etc/hosts.#{k}",
+            :ensure  => :present,
+            :content => template(File.join(File.dirname(__FILE__), '..', '..', 'templates', "hosts.#{k}.erb"), binding)
+        end
       end
     end
   end
